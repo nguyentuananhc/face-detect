63812217795580bd4a1e4255e61aea170454ce66
Tuan Anh <ntanh0794@gmail.com>
2019-10-17 13:26:48 +0700

    1st commit

---
+++ script.js
@@ -0,0 +1,45 @@
+'use strict';
+
+var constraints = {
+  video: true
+};
+
+const video = document.getElementById('video')
+
+function handleSuccess(stream) {
+  window.stream = stream; // only to make stream available to console
+  video.srcObject = stream;
+}
+
+function handleError(error) {
+  console.log('getUserMedia error: ', error);
+}
+
+const startVideo = () => {
+  navigator.mediaDevices.getUserMedia(constraints).
+    then(handleSuccess).catch(handleError);
+}
+
+// startVideo()
+
+Promise.all([
+  faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+  faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+  faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+  faceapi.nets.faceExpressionNet.loadFromUri('/models')
+]).then(startVideo)
+
+video.addEventListener('play', () => {
+  const canvas = faceapi.createCanvasFromMedia(video)
+  document.body.append(canvas)
+  const displaySize = { width: video.width, height: video.height - 150 }
+  faceapi.matchDimensions(canvas, displaySize)
+  setInterval(async () => {
+    const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions()).withFaceLandmarks().withFaceExpressions()
+    const resizedDetections = faceapi.resizeResults(detections, displaySize)
+    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
+    faceapi.draw.drawDetections(canvas, resizedDetections)
+    faceapi.draw.drawFaceLandmarks(canvas, resizedDetections)
+    faceapi.draw.drawFaceExpressions(canvas, resizedDetections)
+  }, 100)
+})
